41cf4a817c60f9fa2e459430d0ab2df88e437edf
Andy Chiu <achiu8@gmail.com>
2014-12-13 15:07:42 -0800

    Add Yelp API and post route

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,6 +1,23 @@
+require 'yelp'
+
+$yelp = Yelp::Client.new({
+  consumer_key: "ABHseWQ2ikV5ke6mpU-6-A",
+  consumer_secret: "2EFEJD4UYc2k9D_nhfUZU3BYszA",
+  token: "U8l06FytOYvxYf68d-EFPl-L7buOKTEa",
+  token_secret: "vWr4aIV3iwgib9F9mrTHKFTob_E"
+})
 
 get '/' do
   File.read(File.join('public', 'index.html'))
 end
 
+post '/search' do
+  bounding_box = {
+    sw_latitutde: params[:sw_lat],
+    sw_longitude: params[:sw_long],
+    ne_latitutde: params[:ne_lat],
+    ne_longitude: params[:ne_long],
+  }
 
+  results = $yelp.search_by_bounding_box(bounding_box, { term: params[:term] })
+end
