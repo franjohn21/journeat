9d0866dc0c8d05dd756fee4bce910fb51b1febd4
Francis John <francis.john@colorado.edu>
2014-12-13 17:59:30 -0800

    Fix radius

--- public/js/application.js
+++ public/js/application.js
@@ -33,13 +33,13 @@ journeatApp.controller('journeatCtrl', function($scope) {
 
   var mapOptions = {
     center: new google.maps.LatLng(37.7833, -122.4167),
-    zoom: 6
+    zoom: 12
   };
 
   var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
   var directionsDisplay = new google.maps.DirectionsRenderer();
   var directionsService = new google.maps.DirectionsService();
-
+  var numrequests = 15
   directionsDisplay.setMap(map);
 
   $scope.handleQuery = function() {
@@ -59,9 +59,10 @@ journeatApp.controller('journeatCtrl', function($scope) {
         var coords = [
           {latitude: startlat, longitude: startlng}
         ]
-        var radius = response.routes[0].legs[0].distance.value/6
-        for(var i = response.routes[0].overview_path.length/5; i < response.routes[0].overview_path.length; i += response.routes[0].overview_path.length/5)
+        var radius = 1000
+        for(var i = Math.floor(response.routes[0].overview_path.length/numrequests); i < response.routes[0].overview_path.length; i += Math.floor(response.routes[0].overview_path.length/numrequests))
         {
+          console.log(i)
           var latitude = response.routes[0].overview_path[i].k
           var longitude = response.routes[0].overview_path[i].D
           coords.push({latitude: latitude, longitude: longitude})
